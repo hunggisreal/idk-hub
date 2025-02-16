53ea518339f43f5ee760c4af4d5435a4427ca13d
hunggchuaii <hunggchuaii@gmail.com>
2025-02-16 19:44:03 +0700

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,2 +1,4 @@
 if game.PlaceId = 70876832253163 then
-  loadstring(game:HttpGet(""))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/hunggchuaii/rblx/refs/heads/main/DeadRailsInGame.lua"))()
+end
+if game.PlaceId = 
