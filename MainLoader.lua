c773e1dfc6ec0f0ae310e2be88ab6202e81e8c6d
hunggisreal <hunggchuaii@gmail.com>
2025-03-26 20:14:47 +0700

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,15 +1,29 @@
 if game.PlaceId == 116495829188952 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/hunggchuaii/my-script/refs/heads/main/DeadRailsLobby.lua"))()
-else
-if game.PlaceId == 70876832253163 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/hunggchuaii/my-script/refs/heads/main/DeadRails.lua"))()
-else
-     local StarterGui = game:GetService("StarterGui")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/hunggisreal/my-script/refs/heads/main/DeadRailsLobby.lua"))()
 
-StarterGui:SetCore("SendNotification", {
-    Title = "epik notify";
-    Text = "Game Not Supported!❌";
-    Icon = "rbxassetid://104451507735118";
-    Duration = 5;
-})
+    local StarterGui = game:GetService("StarterGui")
+    StarterGui:SetCore("SendNotification", {
+        Title = "Status:✅";
+        Text = "Game Found: Dead Rails";
+        Icon = "rbxassetid://104451507735118";
+        Duration = 5;
+    })
+elseif game.PlaceId == 70876832253163 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/hunggisreal/my-script/refs/heads/main/DeadRails.lua"))()
+    
+    local StarterGui = game:GetService("StarterGui")
+    StarterGui:SetCore("SendNotification", {
+        Title = "Status:✅";
+        Text = "Game Found: Dead Rails";
+        Icon = "rbxassetid://104451507735118";
+        Duration = 5;
+    })
+else
+    local StarterGui = game:GetService("StarterGui")
+    StarterGui:SetCore("SendNotification", {
+        Title = "Status:❌";
+        Text = "Game Not Supported";
+        Icon = "rbxassetid://104451507735118";
+        Duration = 5;
+    })
 end
