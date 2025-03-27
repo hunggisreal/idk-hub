e7732982ad990480f64625ab048d16aa210e56d1
hunggisreal <hunggchuaii@gmail.com>
2025-03-27 10:08:03 +0700

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,7 +1,7 @@
 -- you can use this code to load my script: loadstring(game:HttpGet("https://raw.githubusercontent.com/hunggisreal/my-script/refs/heads/main/MainLoader.lua"))()
 
 if game.PlaceId == 116495829188952 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/hunggisreal/my-script/refs/heads/main/DeadRailsLobby.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/hunggisreal/my-script/refs/heads/main/DeadRails.lua"))()
 
     local StarterGui = game:GetService("StarterGui")
     StarterGui:SetCore("SendNotification", {
