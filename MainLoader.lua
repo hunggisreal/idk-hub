c053d83dcb378dd834e116f78c115543b7d262e8
hunggisreal <hunggchuaii@gmail.com>
2025-03-29 21:14:10 +0700

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,5 +1,14 @@
 -- you can use this code to load my script: loadstring(game:HttpGet("https://raw.githubusercontent.com/hunggisreal/my-script/refs/heads/main/MainLoader.lua"))()
-
+if game.PlaceId == 18843545945 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/hunggisreal/my-script/refs/heads/main/BeADog.lua"))()
+local StarterGui = game:GetService("StarterGui")
+    StarterGui:SetCore("SendNotification", {
+        Title = "Status:✅";
+        Text = "Game Found: Be A Dog";
+        Icon = "rbxassetid://104451507735118";
+        Duration = 5;
+    })
+end
 if game.PlaceId == 116495829188952 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/hunggisreal/my-script/refs/heads/main/DeadRails.lua"))()
 
