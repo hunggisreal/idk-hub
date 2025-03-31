d62dde3b460f4f1071612dd92600dadbc92cf60f
hunggisreal <hunggchuaii@gmail.com>
2025-03-31 07:58:51 +0700

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,14 +1,4 @@
 -- you can use this code to load my script: loadstring(game:HttpGet("https://raw.githubusercontent.com/hunggisreal/my-script/refs/heads/main/MainLoader.lua"))()
-if game.PlaceId == 18843545945 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/hunggisreal/my-script/refs/heads/main/BeADog.lua"))()
-local StarterGui = game:GetService("StarterGui")
-    StarterGui:SetCore("SendNotification", {
-        Title = "Status:✅";
-        Text = "Game Found: Be A Dog";
-        Icon = "rbxassetid://104451507735118";
-        Duration = 5;
-    })
-end
 if game.PlaceId == 116495829188952 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/hunggisreal/my-script/refs/heads/main/DeadRails.lua"))()
 
@@ -26,6 +16,15 @@ elseif game.PlaceId == 70876832253163 then
     StarterGui:SetCore("SendNotification", {
         Title = "Status:✅";
         Text = "Game Found: Dead Rails";
+        Icon = "rbxassetid://104451507735118";
+        Duration = 5;
+    })
+elseif game.PlaceId == 18843545945 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/hunggisreal/my-script/refs/heads/main/BeADog.lua"))()
+    local StarterGui = game:GetService("StarterGui")
+    StarterGui:SetCore("SendNotification", {
+        Title = "Status:✅";
+        Text = "Game Found: Be A Dog";
         Icon = "rbxassetid://104451507735118";
         Duration = 5;
     })
